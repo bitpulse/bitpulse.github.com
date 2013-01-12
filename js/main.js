6b9eee181f227dc7e7dc4d3728e222e419b82adb
Martin Høst Normark <m@martinnormark.com>
2013-01-12 20:46:40 +0100

    Minor adjustments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 (function() {
 
 	$(document).ready(function() {
+		$("#parallax .parallax-layer")
+		.parallax({
+		  mouseport: $("#parallax")
+		});
+		
 		mixpanel.track_forms("#subForm", "invite form submission");
 	});
 
