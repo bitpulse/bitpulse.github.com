e9026d584e403fdc702a53e654522512466084c0
Martin Høst Normark <m@martinnormark.com>
2013-01-12 21:14:56 +0100

    Tweak circle movement

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 	$(document).ready(function() {
 		$("#parallax .parallax-layer")
 		.parallax({
-		  mouseport: $(".wrapper")
+		  mouseport: $(".wrapper"),
+		  decay: 0.3
 		});
 		
 		mixpanel.track_forms("#subForm", "invite form submission");
